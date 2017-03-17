a6973be60f4652f9b8cf861eef078bc18cee0429
Justin Bell <justin.bell@mail.mcgill.ca>
2017-03-17 16:27:47 -0400

    Change generic console log

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ app.post('/card/:id/credit', function (req, res) {
 });
 
 
-
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('decode-giftcards server listening on port 3000!');
 });
